b8adabc38b654ab226f5a147f0a48dea6652cac9
iber <iberson-silva@outlook.com>
2024-09-07 17:10:37 -0500

    agregar puerto...

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+  
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
 const client = new Client({
     puppeteer: {
